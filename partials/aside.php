a56a7e61285b1a4a12eff23bfc0b31fdc0a13157
Walter Kimaro <wakyj07@gmail.com>
2018-06-13 12:18:38 +0300

    Beautifying sidebar

--- partials/aside.php
+++ partials/aside.php
@@ -16,34 +16,41 @@
         </li>
         <li>
             <a href="index2.php">
-                <i class="zmdi zmdi-edit"></i>Dashboard
-            </a>
-        </li>
-        <li>
-            <a href="create_memo.php">
-                <i class="zmdi zmdi-edit"></i> Create Memo
+                <i class="zmdi zmdi-view-dashboard"></i>Dashboard
             </a>
         </li>
 
-        <li>
-            <a href="view_mymemo.php">
-            <i class="zmdi zmdi-edit"></i> View Memo
+        <li class="dropdown active">
+            <a href="#">
+                <i class="zmdi zmdi-file"></i>
+                My Memos
             </a>
+
+            <ul class="dropdown-menu">
+                <li>
+                    <a href="create_memo.php">Create Memo</a>
+                </li>
+                <li>
+                    <a href="view_mymemo.php">View Memo</a>
+                </li>
+                <!-- <li><a href="memo-drafts.html">Memo Drafts</a></li> -->
+            </ul>
         </li>
+
         <li>
             <a href="view_dept.php">
-                <i class="zmdi zmdi-edit"></i> Departments
+                <i class="zmdi zmdi-accounts-list-alt"></i> Departments
             </a>
         </li>
         <li>
             <a href="view_jobs.php">
-                <i class="zmdi zmdi-edit"></i> Job Titles
+                <i class="zmdi zmdi-case"></i> Job Titles
             </a>
         </li>
 
         <li>
             <a href="staff.php">
-                <i class="zmdi zmdi-edit"></i> Staff List
+                <i class="zmdi zmdi-accounts"></i> Staff List
             </a>
         </li>
         <li>
