e191ead2c48495c184d4bd0bd5b77f6ede4c08f3
Walter Kimaro <wakyj07@gmail.com>
2018-07-11 06:56:24 +0300

    Added alert modal

--- partials/reply-options-modal.php
+++ partials/reply-options-modal.php
@@ -1,26 +1,22 @@
-<style>
-    
-</style>
-<div id="replyOptions" class="modal sm">
-    <div class="modal-content" style="min-width: 260px; padding-bottom: 0; padding-top: 0;">
+<div id="replyOptions" class="modal alert">
+    <div class="modal-content">
         <div class="modal-title">Pick An Option</div>
-        <div class="modal-body" style="padding: 0;">
-            <div class="layout center" onclick="closeModal('replyOptions'); sendReply(0)" style="padding: 0.5em 1em">
-                <div class="layout center-center" style="position: relative; background: #2fb92f; margin-right: 12px; width: 40px; height: 40px; border-radius: 50px;">
+        <div class="modal-body">
+            <div class="option-item" onclick="closeModal('replyOptions'); sendReply(0)">
+                <div class="icon" style="background: #2fb92f;">
                     <i class="zmdi zmdi-check"></i>
                 </div>
                 <p class="flex text-">Approve</p>
             </div>
 
-            <div class="layout center" onclick="closeModal('replyOptions'); sendReply(0)" style="padding: 0.5em 1em">
-                <div class="layout center-center" style="position: relative; background: #e83c3d; margin-right: 12px; width: 40px; height: 40px; border-radius: 50px;">
+            <div class="option-item" onclick="closeModal('replyOptions'); sendReply(0)">
+                <div class="icon layout center-center" style="background: #e83c3d;">
                     <i class="zmdi zmdi-close"></i>
                 </div>
                 <p class="flex text-">Decline</p>
             </div>
 
-            <div class="layout center-center" onclick="closeModal('replyOptions'); openModal('writeReply');"
-            style="background: black; color: #fff; padding: 1em;">
+            <div class="option-item full" onclick="closeModal('replyOptions'); openModal('writeReply');">
                 <i class="zmdi zmdi-edit"></i> &emsp;&nbsp;
                 <p class="text-">Write A Comment</p>
             </div>
