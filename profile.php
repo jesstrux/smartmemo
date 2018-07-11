a73df4cfd2843e5be1cd63e4d80e7d746e35e153
Walter Kimaro <wakyj07@gmail.com>
2018-07-11 10:07:57 +0300

    Profile page

--- profile.php
+++ profile.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+    $no_bootstrap = true;
     include("includes/connection.php");
     include("partials/header.php");
     include('includes/getDepartment.php');
@@ -18,15 +19,37 @@
     <div id="siteContent" class="flex">
         <?php include("partials/navbar.php");?>
         <div id="mainContent">
+            <section class="page-title layout vertical center-center">
+                <i class="zmdi zmdi-account"></i>
+                <h1 class="text-light">
+                    <?php echo $user['fullname']; ?>
+                </h1>
+            </section>
             <div class="section-wrapper">
-                <!-- Page content goes here-->
-                <div class="panel-body">
-                    <label>Fullname: <?php echo $user['fullname']; ?></label><br>
-                    <label>Email   : <?php echo $user['email']; ?></label><br>
-                    <label>Phone number: <?php echo $user['phoneNumber']; ?></label><br>
-                    <label>Department: <?php echo getDepartment::getdept($con,$user['dept_id']); ?></label><br>
-                    <label>Job Title: <?php echo getJob::getjobs($con,$user['job_id']); ?></label><br>
-                </div>
+                <table class="table" style="margin-top: -1em;max-width: 1000px; margin: auto">
+                    <thead>
+                        <tr>
+                            <th>FIRST NAME</th>
+                            <th>MIDDLE NAME</th>
+                            <th>LAST NAME</th>
+                            <th>EMAIL</th>
+                            <th>PHONE</th>
+                            <th>DEPARTMENT</th>
+                            <th>POSITION</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><?php echo $user['fname']; ?></td>
+                            <td><?php echo $user['mname']; ?></td>
+                            <td><?php echo $user['surname']; ?></td>
+                            <td><?php echo $user['email']; ?></td>
+                            <td><?php echo $user['phoneNumber']; ?></td>
+                            <td><?php echo getDepartment::getdept($con, $user['dept_id']); ?></td>
+                            <td><?php echo getJob::getjobs($con, $user['job_id']); ?></label>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
             <footer class="layout center-justified">
                 Coyright &copy; Smart Memo 2018
